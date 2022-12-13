67b1a72c5bf0b8a8708ee5d5eec4c0ca9f52d0f7
AlvinNjenga <109256320+AlvinNjenga@users.noreply.github.com>
2022-12-13 00:24:07 +0000

    Created the first files

---
+++ FunnyForecasts/ForecastService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace FunnyForecasts
+{
+    public class ForecastService
+    {
+        HttpClient _httpClient { get; set; }
+        public ForecastService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public void GetRandomApiCall()
+        {
+
+        }
+    }
+}
